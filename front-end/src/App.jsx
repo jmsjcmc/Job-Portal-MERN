7d157f33908cbd8cc9bc3d6c7862c7036cf7c394
jmsjcmc <jecemecojamestabilog@gmail.com>
2025-08-13 16:20:45 +0800

    Add new pages for Dashboard, AddJob, ManageJobs, and ViewApplications; update App component routes

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -6,6 +6,10 @@ import Applications from './pages/Applications'
 import RecruiterLogin from './components/RecruiterLogin'
 import { useContext } from 'react'
 import { AppContext } from './context/AppContext'
+import Dashboard from './pages/Dashboard'
+import AddJob from './pages/AddJob'
+import ManageJobs from './pages/ManageJobs'
+import ViewApplications from './pages/ViewApplications'
 
 export default function App() {
   const {showRecruiterLogin} = useContext(AppContext)
@@ -16,6 +20,10 @@ export default function App() {
         <Route path='/' element={<Home/>}/>
         <Route path='/apply-job/:id' element={<ApplyJob/>}/>
         <Route path='/applications' element={<Applications/>}/>
+        <Route path='/dashboard' element={<Dashboard/>}/>
+        <Route path='add-job' element={<AddJob/>}/>
+        <Route path='manage-jobs' element={<ManageJobs/>}/>
+        <Route path='view-applications' element={<ViewApplications/>}/>
       </Routes>
     </div>
   )
